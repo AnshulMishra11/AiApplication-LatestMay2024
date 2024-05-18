a0bcc7f098b572fa0024e494d53c11b1233a8220
Anshul <becomeytr@gmail.com>
2024-05-19 04:12:13 +0530

    second

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,7 +7,7 @@ const Head = () => {
     <div className="footer flex flex-row justify-between p-4 bg-gray-200">
       <div>Developed by Anshul Mishra [More changes will be done Soon ..] </div>
       <div>
-        <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <a href="https://www.linkedin.com/in/anshul-mishra-756557250/" style={{ textDecoration: 'none', color: 'inherit' }}>
           <InfoIcon />
         </a>
       </div>
